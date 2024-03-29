6982a300732db784cb35e2d821ee589f981bdd05
Symm <155481577+SymmGitHub@users.noreply.github.com>
2024-03-29 18:02:45 -0400

    Add files via upload
    Updated forms with very rudimentary buttons and mod loading features.

---
+++ ModManager/CCModManager.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace ModManager
+{
+    partial class CCModManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCCDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.joinTheModdingDiscordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.modList = new System.Windows.Forms.ListView();
+            this.columnMain = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.importMods = new System.Windows.Forms.Button();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.mainProgress = new System.Windows.Forms.ProgressBar();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(920, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCCDirectoryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 26);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openCCDirectoryToolStripMenuItem
+            // 
+            this.openCCDirectoryToolStripMenuItem.Name = "openCCDirectoryToolStripMenuItem";
+            this.openCCDirectoryToolStripMenuItem.Size = new System.Drawing.Size(296, 26);
+            this.openCCDirectoryToolStripMenuItem.Text = "Open Castle Crashers Directory";
+            this.openCCDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openCCDirectoryToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lightModeToolStripMenuItem,
+            this.darkModeToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(76, 26);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // lightModeToolStripMenuItem
+            // 
+            this.lightModeToolStripMenuItem.CheckOnClick = true;
+            this.lightModeToolStripMenuItem.Name = "lightModeToolStripMenuItem";
+            this.lightModeToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.lightModeToolStripMenuItem.Text = "Light Mode";
+            this.lightModeToolStripMenuItem.CheckedChanged += new System.EventHandler(this.switchDisplayColorToolStripMenuItem_CheckedChanged);
+            // 
+            // darkModeToolStripMenuItem
+            // 
+            this.darkModeToolStripMenuItem.CheckOnClick = true;
+            this.darkModeToolStripMenuItem.Name = "darkModeToolStripMenuItem";
+            this.darkModeToolStripMenuItem.Size = new System.Drawing.Size(168, 26);
+            this.darkModeToolStripMenuItem.Text = "Dark Mode";
+            this.darkModeToolStripMenuItem.CheckedChanged += new System.EventHandler(this.switchDisplayColorToolStripMenuItem_CheckedChanged);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.joinTheModdingDiscordToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // joinTheModdingDiscordToolStripMenuItem
+            // 
+            this.joinTheModdingDiscordToolStripMenuItem.Name = "joinTheModdingDiscordToolStripMenuItem";
+            this.joinTheModdingDiscordToolStripMenuItem.Size = new System.Drawing.Size(263, 26);
+            this.joinTheModdingDiscordToolStripMenuItem.Text = "Join the Modding Discord";
+            this.joinTheModdingDiscordToolStripMenuItem.Click += new System.EventHandler(this.joinTheModdingDiscordToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(263, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BackgroundImage = global::ModManager.Properties.Resources.CCModManagerLogo1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 54);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(632, 75);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(20, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enabled Mods";
+            // 
+            // modList
+            // 
+            this.modList.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.modList.AllowColumnReorder = true;
+            this.modList.CheckBoxes = true;
+            this.modList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnMain});
+            this.modList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.modList.FullRowSelect = true;
+            this.modList.HideSelection = false;
+            this.modList.LabelWrap = false;
+            this.modList.Location = new System.Drawing.Point(24, 156);
+            this.modList.Name = "modList";
+            this.modList.Size = new System.Drawing.Size(665, 427);
+            this.modList.TabIndex = 3;
+            this.modList.UseCompatibleStateImageBehavior = false;
+            this.modList.View = System.Windows.Forms.View.List;
+            // 
+            // columnMain
+            // 
+            this.columnMain.Width = 650;
+            // 
+            // importMods
+            // 
+            this.importMods.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.importMods.Location = new System.Drawing.Point(695, 156);
+            this.importMods.Name = "importMods";
+            this.importMods.Size = new System.Drawing.Size(215, 37);
+            this.importMods.TabIndex = 4;
+            this.importMods.Text = "Import Mods";
+            this.importMods.UseVisualStyleBackColor = true;
+            this.importMods.Click += new System.EventHandler(this.importMods_Click);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(21, 590);
+            this.progressLabel.MinimumSize = new System.Drawing.Size(665, 0);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(665, 16);
+            this.progressLabel.TabIndex = 5;
+            this.progressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // mainProgress
+            // 
+            this.mainProgress.Location = new System.Drawing.Point(24, 615);
+            this.mainProgress.Name = "mainProgress";
+            this.mainProgress.Size = new System.Drawing.Size(312, 20);
+            this.mainProgress.TabIndex = 6;
+            // 
+            // CCModManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::ModManager.Properties.Resources.ModManagerBG1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(920, 641);
+            this.Controls.Add(this.mainProgress);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.importMods);
+            this.Controls.Add(this.modList);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "CCModManager";
+            this.Text = "Castle Crashers Mod Manager v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openCCDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem joinTheModdingDiscordToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView modList;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkModeToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnMain;
+        private System.Windows.Forms.Button importMods;
+        private System.Windows.Forms.Label progressLabel;
+        private System.Windows.Forms.ProgressBar mainProgress;
+    }
+}
+
---
+++ ModManager/CCModManager.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModManager
+{
+    public partial class CCModManager : Form
+    {
+        bool loading;
+        string modFolder, backupFolder, dataFolder;
+        bool lightMode;
+        public CCModManager()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loading = true;
+
+            // Define paths for Castle Crashers' Vanilla Data and Mods
+            modFolder = AppDomain.CurrentDomain.BaseDirectory + "\\Mods";
+            backupFolder = AppDomain.CurrentDomain.BaseDirectory + "\\Backup";
+
+            // Load Settings
+            dataFolder = Properties.Settings.Default.ccDataPath;
+            lightMode = Properties.Settings.Default.lightMode;
+            UpdateDisplayType();
+
+            if (Directory.Exists(modFolder))
+            {
+                foreach (string folder in  Directory.GetDirectories(modFolder))
+                {
+                    // Add new mod to list
+                    modList.Items.Add(ParseMod(folder));
+                }
+            }
+            else Directory.CreateDirectory(modFolder);
+            loading = false;
+        }
+        private ListViewItem ParseMod(string path)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = Path.GetFileName(path);
+            return item;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 box = new AboutBox1();
+            box.ShowDialog();
+        }
+
+        public void UpdateDisplayType()
+        {
+            // Update the menu strip buttons
+            lightModeToolStripMenuItem.Enabled = !lightMode;
+            lightModeToolStripMenuItem.Checked = lightMode;
+            darkModeToolStripMenuItem.Enabled = lightMode;
+            darkModeToolStripMenuItem.Checked = !lightMode;
+
+            // TO DO: Actually uhhhh make this work by updating all the UI stuff
+        }
+
+        private void openCCDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Please select the location of the 'data' folder used by Castle Crashers" + Environment.NewLine +
+                "This will also make a backup of the files for restoring them to vanilla.");
+
+            FolderBrowserDialog open = new FolderBrowserDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                dataFolder = open.SelectedPath;
+                Properties.Settings.Default.ccDataPath = dataFolder;
+                Properties.Settings.Default.Save();
+                if (Directory.Exists(backupFolder)) Directory.Delete(backupFolder, true);
+                mainProgress.Value = 0;
+                mainProgress.Maximum = DirectoryFileMeasurement(dataFolder, false);
+                progressLabel.Text = "Saving files to 'Backup' Folder...";
+                CopyDirectory(dataFolder, backupFolder, false);
+            }
+        }
+
+        private void switchDisplayColorToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            // Use loading bool to prevent this function from repeating itself
+            if (loading) return;
+            loading = true;
+            lightMode = !lightMode;
+            Properties.Settings.Default.lightMode = lightMode;
+            Properties.Settings.Default.Save();
+            UpdateDisplayType();
+            loading = false;
+        }
+
+        private void importMods_Click(object sender, EventArgs e)
+        {
+            // Restore backup folder before importing mods
+            if (!Directory.Exists(backupFolder))
+            {
+                // If there is no backup made, prompt user to make one first
+                openCCDirectoryToolStripMenuItem_Click(sender, e);
+                return;
+            }
+
+            if (Directory.Exists(dataFolder) && dataFolder.EndsWith("data")) 
+            {
+                // Warn the user if more than one mod is checked
+                if (modList.CheckedItems.Count > 1)
+                {
+                    DialogResult dialogResult = MessageBox.Show(
+                        $"Due to many mods having files that overlap, it's best" + Environment.NewLine +
+                        $"to only use one mod at a time, install multiple?", "", MessageBoxButtons.YesNo);
+                    if (dialogResult != DialogResult.Yes) return;
+                }
+
+                Directory.Delete(dataFolder, true);
+                mainProgress.Value = 0;
+                mainProgress.Maximum = DirectoryFileMeasurement(backupFolder, false);
+                progressLabel.Text = "Restoring original files...";
+                CopyDirectory(backupFolder, dataFolder, false);
+                foreach (ListViewItem item in modList.CheckedItems)
+                {
+                    // Import each mod
+                    mainProgress.Value = 0;
+                    mainProgress.Maximum = DirectoryFileMeasurement(modFolder + $"\\{item.Text}", true);
+                    progressLabel.Text = $"Installing '{item.Text}'...";
+                    CopyDirectory(modFolder + $"\\{item.Text}", dataFolder, true);
+                }
+            }
+            else
+            {
+                // If no data directory is found, or doesn't end with 'data', show error.
+                throw new DirectoryNotFoundException($"Failed to find Castle Crashers 'data' folder");
+            }
+        }
+
+        private void joinTheModdingDiscordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        void CopyDirectory(string sourceDir, string destinationDir, bool directoriesOnly)
+        {
+            if (!Directory.Exists(destinationDir)) Directory.CreateDirectory(destinationDir);
+            var dir = new DirectoryInfo(sourceDir);
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            if (!directoriesOnly)
+            {
+                foreach (string file in Directory.GetFiles(sourceDir))
+                {
+                    string targetFilePath = Path.Combine(destinationDir, Path.GetFileName(file));
+                    progressLabel.Text = $"Copying File: {Path.GetFileName(file)}";
+                    File.Copy(file, targetFilePath, true);
+                    mainProgress.Value++;
+                    if (mainProgress.Value == mainProgress.Maximum) progressLabel.Text = "Done!";
+                    progressLabel.Refresh();
+                }
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir, false);
+            }
+        }
+        static int DirectoryFileMeasurement(string dir, bool directoriesOnly)
+        {
+            int count = 0;
+            if (!directoriesOnly) count += Directory.GetFiles(dir).Length;
+            foreach (string subDir in Directory.GetDirectories(dir))
+            {
+                count += DirectoryFileMeasurement(subDir, false);
+            }
+            return count;
+        }
+    }
+}
